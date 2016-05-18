0daede4872514e83ac2de9360065b8ec021106a2
Simon <Beatsleigher@gmail.com>
2016-05-18 17:20:37 +0200

    Still working on fixing the bug...

--- src/Hangman_IFA51/words.c
+++ src/Hangman_IFA51/words.c
@@ -49,28 +49,34 @@ struct WordCategories getWords() {
         for (int i = 0; i < sizeof(filePaths) / sizeof(filePaths[0]); i++) {
                 int result;
                 char canonPath[256];
+                char statusMsg[256];
+
+                clearBottomLine();
+
                 switch (i) {
                     case 0:
                         // Level easy
                         _fullpath(canonPath, "..\\words\\level_easy", 265); // "Implicit declaration". BULLSHIT. It's located in stdlib.h!
-                        //printf("%s...", canonPath);
+                        //s//printf(statusMsg, "%s...", canonPath);
+                        //printBottomCentre(statusMsg);
                         if (access(canonPath, F_OK) != -1) {
-                            //printf("Exists!\n");
+                            ////printf("Exists!\n");
                         } else {
                             //printf("Doesn't exist!\n");
                         }
                         result = CopyFile(canonPath, filePaths[i], true);
                         //printf("Copying to file %s", filePaths[i]);
                         if (result <= 0) {
-                            printf("...FAILED!\n");
+                            //printf("...FAILED!\n");
                         } else {
-                            printf("...SUCCESS!\n");
+                            //printf("...SUCCESS!\n");
                         }
                         break;
                     case 1:
                         // Level medium
                         _fullpath(canonPath, "..\\words\\level_medium", 265); // Same here
-                        //printf("%s...", canonPath);
+                        //s//printf(statusMsg, "%s...", canonPath);
+                        //printBottomCentre(statusMsg);
                         if (access(canonPath, F_OK) != -1) {
                             //printf("Exists!\n");
                         } else {
@@ -79,15 +85,16 @@ struct WordCategories getWords() {
                         result = CopyFile(canonPath, filePaths[i], true);
                         //printf("Copying to file %s", filePaths[i]);
                         if (result <= 0) {
-                            printf("...FAILED!\n");
+                            //printf("...FAILED!\n");
                         } else {
-                            printf("...SUCCESS!\n");
+                            //printf("...SUCCESS!\n");
                         }
                         break;
                     case 2:
                         // Level hard
                         _fullpath(canonPath, "..\\words\\level_hard", 265);
-                        //printf("%s...", canonPath);
+                        //s//printf(statusMsg, "%s...", canonPath);
+                        //printBottomCentre(statusMsg);
                         if (access(canonPath, F_OK) != -1) {
                             //printf("Exists!\n");
                         } else {
@@ -96,15 +103,16 @@ struct WordCategories getWords() {
                         result = CopyFile(canonPath, filePaths[i], true);
                         //printf("Copying to file %s", filePaths[i]);
                         if (result <= 0) {
-                            printf("...FAILED!\n");
+                            //printf("...FAILED!\n");
                         } else {
-                            printf("...SUCCESS!\n");
+                            //printf("...SUCCESS!\n");
                         }
                         break;
                     case 3:
                         // Level insane
                         _fullpath(canonPath, "..\\words\\level_insane", 265);
-                        //printf("%s...", canonPath);
+                        //s//printf(statusMsg, "%s...", canonPath);
+                        //printBottomCentre(statusMsg);
                         if (access(canonPath, F_OK) != -1) {
                             //printf("Exists!\n");
                         } else {
@@ -113,9 +121,9 @@ struct WordCategories getWords() {
                         result = CopyFile(canonPath, filePaths[i], true);
                         //printf("Copying to file %s", filePaths[i]);
                         if (result <= 0) {
-                            printf("...FAILED!\n");
+                            //printf("...FAILED!\n");
                         } else {
-                            printf("...SUCCESS!\n");
+                            //printf("...SUCCESS!\n");
                         }
                         break;
                     default:
@@ -129,7 +137,6 @@ struct WordCategories getWords() {
     for (int i = 0; i < sizeof(filePaths) / sizeof(filePaths[0]); i++) {
 
         FILE *fPointer;
-        char *line = NULL;
         char *read;
         int index = 0;
         char wordContainer[5000][150];
@@ -138,12 +145,52 @@ struct WordCategories getWords() {
 
         fPointer = fopen(filePaths[i], "r");
         if (fPointer == NULL) {
-            printf("Error occurred while opening file!");
+            printError("Error occurred while opening file!");
             exit(0x002);
         }
 
         // Re-coding.
         // Apparently the fucker doesn't work...
+        // (c) Iron@Stackoverflow.com
+        int maximumLineLength = 128;
+        char *lineBuffer = (char *)malloc(sizeof(char) * maximumLineLength);
+
+        if (lineBuffer == NULL) {
+            printError("Error allocating memory for line buffer.");
+            exit(0x003);
+        }
+
+        char ch = getc(fPointer);
+        int count = 0;
+
+        do {
+            while ((ch != '\n') && (ch != EOF)) {
+                if (count == maximumLineLength) {
+                    maximumLineLength += 128;
+                    lineBuffer = realloc(lineBuffer, maximumLineLength);
+                    if (lineBuffer == NULL) {
+                        printError("Error reallocating space for line buffer.");
+                        exit(0x004);
+                    }
+                }
+                lineBuffer[count] = ch;
+                count++;
+
+                ch = getc(fPointer);
+            }
+
+            lineBuffer[count] = '\0';
+            char line[count + 1];
+            strncpy(line, lineBuffer, (count + 1));
+            free(lineBuffer);
+            const char *constLine = line;
+
+            // Test code
+            printRight(constLine, 6);
+            Sleep(750);
+            clearLine(6);
+        } while (ch != EOF);
+
 
 
         fclose(fPointer);
